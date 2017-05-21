32c46b4c1641e992fa15404fd74330791160c192
xisabao <ubiquitoustrees@gmail.com>
2017-05-20 21:56:15 -0700

    add email config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tastebackapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: ENV['GMAIL_DOMAIN'],
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD']
+  }
+   config.action_mailer.default_url_options = { host: 'https://tasteback.herokuapp.com'}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
