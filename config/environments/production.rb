23f48afa3ddc8dc25f1e8b506bd0cdb73d13744d
xisabao <ubiquitoustrees@gmail.com>
2017-05-20 22:16:58 -0700

    fml made a typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {  
   address: "smtp.gmail.com",
   port: 587,
-  domain: "tasteback..herokuapp.com",
+  domain: "tasteback.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
