9af3185c10c54c1ed2d5d2facf5aefc4fc341b08
xisabao <ubiquitoustrees@gmail.com>
2017-05-20 23:42:55 -0700

    try and fix image view issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,9 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     },
-    bucket: 'tasteback-assets'
+    bucket: 'tasteback-assets',
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
